66c5277fd112a04ba28b32c3a9c76c951039a85c
vaibhav sharma <vaibhav.sharma@outlook.com>
2024-11-12 22:17:01 +0530

    workers updated to 8

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import os
 import signal
 import fastapi
 import uvicorn
-from price_server import live_price_app
+from price_server import app
 
 def start():
     return fastapi.Response(status_code=200, content='Server started')
@@ -11,12 +11,12 @@ def shutdown():
     os.kill(os.getpid(), signal.SIGTERM)
     return fastapi.Response(status_code=200, content='Server shutting down...')
 
-@live_price_app.on_event('shutdown')
+@app.on_event('shutdown')
 def on_shutdown():
     print('Server shutting down...')
 
-live_price_app.add_api_route('/start', start, methods=['GET'])
-live_price_app.add_api_route('/shutdown', shutdown, methods=['GET'])
+app.add_api_route('/start', start, methods=['GET'])
+app.add_api_route('/shutdown', shutdown, methods=['GET'])
 
-if __name__ == '__main__':
-    uvicorn.run(live_price_app, host='localhost', port=8006)
+if (__name__ == '__main__'):
+    uvicorn.run('price_server:app', host='localhost', port=8006,workers=8)
--- price_server.py
+++ price_server.py
@@ -3,20 +3,20 @@ from fastapi.responses import JSONResponse
 from base.stock_price import *
 from base.stock_base import *
 
-live_price_app = FastAPI()
+app = FastAPI()
 
-@live_price_app.get('/',response_class=JSONResponse)
+@app.get('/',response_class=JSONResponse)
 def stock_price(request: Request):
     return {'message': 'Price server is running.'}
 
-@live_price_app.get('/{stk_id}/',response_class=JSONResponse)
+@app.get('/{stk_id}/',response_class=JSONResponse)
 def stock_price(request: Request,stk_id:str):
     return {'ticker': get_ticker_info(STK=stk_id,session=None)}
 
-@live_price_app.get('/{stk_id}/price/',response_class=JSONResponse)
+@app.get('/{stk_id}/price/',response_class=JSONResponse)
 def stock_price(request: Request,stk_id:str):
     return {'price': get_stock_price(STK=stk_id,session=None)}
 
-@live_price_app.get('/{stk_id}/{key}/',response_class=JSONResponse)
+@app.get('/{stk_id}/{key}/',response_class=JSONResponse)
 def stock_price(request: Request,stk_id:str,key:str):
     return {f'{key}': get_ticker_info(STK=stk_id,session=None)[key]}

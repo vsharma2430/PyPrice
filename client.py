2fed3005d9fcea15db47e0f1e54a4a3351f25da1
vaibhav sharma <vaibhav.sharma@outlook.com>
2025-02-26 00:10:00 +0530

    user agents error comment added,click commands added

--- client.py
+++ client.py
@@ -1,5 +1,13 @@
 import uvicorn
+import click
 from price_server import app
 
+@click.command
+@click.option('--host',default='localhost')
+@click.option('--port',default=8006)
+@click.option('--dev',default=False)
+def uvicorn_app(host,port,dev):
+    uvicorn.run('price_server:app', host=host, port=port,reload=dev)
+
 if (__name__ == '__main__'):
-    uvicorn.run('price_server:app', host='localhost', port=8006)
+    uvicorn_app()
